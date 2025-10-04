a054c9de6a72659f7d592e9b2499d4b1dc9e115e
lowis9 <wa7shkasr@gmail.com>
2025-10-04 12:17:04 +0300

    Added proper mvc Structure

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <!-- Well begun is half done. - Aristotle -->
+    <h1>Job Board</h1>
+    @foreach($jobs as $job)
+        <div>
+            {{ $job['title'] }} : {{ $job['salary'] }}
+        </div>
+    @endforeach
+</div>
